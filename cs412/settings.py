c8b9e7c26a575c29fd611173e9be538d8f41050c
AshtoshB <ashtosh@bu.edu>
2025-09-12 16:38:12 -0400

    fixed settings

--- cs412/settings.py
+++ cs412/settings.py
@@ -143,7 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # declarations to reference static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/' # note: no leading slash!
 
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, "static")
